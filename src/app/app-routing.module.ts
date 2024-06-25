9fb553b9c4be4093416a6c1b652af50cc3542c95
valenbatata <valusanmartin11@gmail.com>
2024-06-25 10:54:49 -0300

    creación de modulos y capetas, además de el navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+//ruta por defoult que se muestra en la inicialización
+{
+  path:"", component:InicioComponent
+},
+
+
+  // ruta que nos vincula al módulo de inicio y su contenido
+  //loadChildren: Indica que habrá una ruta hija
+  //.then:Función asincronica tipo PROMESA 
+  {
+    path:"",loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
+
+  },
+  {
+    path:"",loadChildren:()=>import('./modules/producto/producto.module').then(m=>m.ProductoModule) 
+  },
+  {
+    path:"",loadChildren:()=>import('./modules/autentificacion/autentificacion.module').then(m=>m.AutentificacionModule) 
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//importación de componentes globales
+import { FooterComponent } from './component/footer/footer.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+
+//Angular components
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
+
+  exports:[
+    FooterComponent,
+    NavbarComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
+  ]
+})
+export class SharedModule { }
